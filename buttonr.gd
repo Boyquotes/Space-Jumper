26a4a9d30424ccdf051dd8b59599fd0b4f09b6b5
lampaBiurkowa <mrocznaklawiatura@gmail.com>
2017-11-29 16:11:09 +0100

    Release 1.0 repaired

---
+++ buttonr.gd
@@ -0,0 +1,9 @@
+extends TextureButton
+
+func _process(delta):
+	if self.is_pressed()&&get_node("/root/Container/player").get_pos().x+get_node("/root/Container/player").get_item_rect().size.x/2<=1020:
+		get_node("/root/Container/player").set_pos(Vector2(get_node("/root/Container/player").get_pos().x+4,get_node("/root/Container/player").get_pos().y))
+
+func _ready():
+	set_process(true)
+	pass
